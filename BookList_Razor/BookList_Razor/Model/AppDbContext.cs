2d3fb9e948a804d1de076561608d4aa5f7b7bfe8
kaleb <kaleb.lucas@aluno.ufabc.edu.br>
2019-05-21 17:16:10 -0300

    added: primeiro commit

---
+++ BookList_Razor/BookList_Razor/Model/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookList_Razor.Model
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
